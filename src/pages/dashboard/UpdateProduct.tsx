84dac44869bd6a423ef99428ca9cdd8af3a33f8a
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-09-04 10:09:02 +0600

    error fixed

--- src/pages/dashboard/UpdateProduct.tsx
+++ src/pages/dashboard/UpdateProduct.tsx
@@ -172,7 +172,6 @@ const UpdateProduct = () => {
                 </div>
 
                 <button
-                    disabled={!img}
                     type="submit"
                     className="bg-green-900 text-white w-44 px-4 py-2 rounded-full hover:bg-green-700 transition duration-200"
                 >
